3504b3cfa89609985245d6feadb05a6fe117696f
allstraws <amitbansal.dtu@gmail.com>
2015-10-21 22:40:16 +0530

    Instagram APIClient

--- restservice/src/main/java/com/engage/api/wrapper/InstaAPIWrapper.java
+++ restservice/src/main/java/com/engage/api/wrapper/InstaAPIWrapper.java
@@ -1,56 +1,37 @@
 package com.engage.api.wrapper;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.HashMap;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-
-public class CallAPI(String api, HashMap<String, String>()){
-	api + forEach(string, value)->append
-	//return JSON;
-}
+//public class CallAPI(String api, HashMap<String, String>()){
+//	api + forEach(string, value)->append
+//	//return JSON;
+//}
 
 public class InstaAPIWrapper {
 
 	public static void main(String[] args) {
-		String resourceURL = "https://api.instagram.com/v1/users/1981378059/?access_token=1981378059.47b3f0d.b673deeeedf941d294d383aa6db9da59";
+//		String resourceURL = "https://api.instagram.com/v1/users/1981378059/?access_token=1981378059.47b3f0d.b673deeeedf941d294d383aa6db9da59";
+		
+		String url = null;
+        HashMap<String, String> postData = new HashMap<>();
+        postData.put("access_token", "1981378059.47b3f0d.b673deeeedf941d294d383aa6db9da59");
+        
+        url = InstaAPIEndPoints.SCHEME_HTTPS + InstaAPIEndPoints.TIMELINE_URL;
 
-		HttpGet get = new HttpGet(resourceURL);
-		DefaultHttpClient client = new DefaultHttpClient();
-		HttpResponse response = null;
-		int code = -1;
-		try {
-			response = client.execute(get);
-			if(response.getEntity().getContentLength() > 0) {
-				StringBuilder sb = new StringBuilder();
-				try {
-				    BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()), 65728);
-				    String line = null;
-
-				    while ((line = reader.readLine()) != null) {
-				        sb.append(line);
-				    }
-				}
-				catch (IOException e) { e.printStackTrace(); }
-				catch (Exception e) { e.printStackTrace(); }
-
-				System.out.println("finalResult " + sb.toString());
-			}
-			code = response.getStatusLine().getStatusCode();
-			
-		} catch (ClientProtocolException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			get.releaseConnection();
-		}
+//        try {
+//            apiClient.writePostData(apiClient.getPostDataString1(postData));
+//        }catch (Exception e){
+//            Log.d("SERVERUTILITIES/POST", "JsonException");
+//        }
+        String response = null;
+        try{
+            ApiClient apiClient = new ApiClient(ApiClient.CallType.HTTPS_GET, url, postData, false);
+        	response = apiClient.executeCall();
+            apiClient.disconnect();
+        } catch(Exception e) {
+        	e.printStackTrace();
+        }
+        if(response != null)
+        	System.out.println(response);
 	}
-	
 }
