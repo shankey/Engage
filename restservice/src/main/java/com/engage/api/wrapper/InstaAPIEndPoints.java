77dd6c7f8f19c5d279420cd49276eae9ddc6c4e6
allstraws <amitbansal.dtu@gmail.com>
2015-10-21 22:56:25 +0530

    Include the base url in http call

--- restservice/src/main/java/com/engage/api/wrapper/InstaAPIEndPoints.java
+++ restservice/src/main/java/com/engage/api/wrapper/InstaAPIEndPoints.java
@@ -5,9 +5,9 @@ public class InstaAPIEndPoints {
 	public static final String SCHEME_HTTPS = "https://";
     public static final String BASE_URL = "api.instagram.com";
 	
-//	public static String TIMELINE_URL = "/v1/users/1981378059/";
+	public static String TIMELINE_URL = "/v1/users/1981378059/";
 
-	public static String TIMELINE_URL = "/v1/users/1981378059/feed";
+//	public static String TIMELINE_URL = "/v1/users/1981378059/feed";
 
 
 }
