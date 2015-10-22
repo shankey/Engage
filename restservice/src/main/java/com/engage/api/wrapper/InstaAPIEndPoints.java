870551f3161c0a407516273ac12260c70de58d8c
allstraws <amitbansal.dtu@gmail.com>
2015-10-22 16:04:19 +0530

    more apis added

--- restservice/src/main/java/com/engage/api/wrapper/InstaAPIEndPoints.java
+++ restservice/src/main/java/com/engage/api/wrapper/InstaAPIEndPoints.java
@@ -8,6 +8,8 @@ public class InstaAPIEndPoints {
     public static final String POST_LIKES = "/v1/media/{media-id}/likes";
     public static final String POST_COMMENTS = "/v1/media/{media-id}/comments";
     public static final String TIMELINE_FEED = "/v1/users/{user-id}/media/recent";
+    public static final String INSTA_FOLLOWERS = "/v1/users/{user-id}/followed-by";
+    public static final String PROFILE_BIO = "/v1/users/{user-id}";
 
 
 	public static final String TIMELINE_URL = "/v1/users/781685528/media/recent";
