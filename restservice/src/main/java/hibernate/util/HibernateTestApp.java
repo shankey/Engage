64c2384c6d035e0c2a1d91f141c53b2e690e236d
Amit <amitbansal.dtu@gmail.com>
2015-10-26 19:08:25 +0530

    Changes

--- restservice/src/main/java/hibernate/util/HibernateTestApp.java
+++ restservice/src/main/java/hibernate/util/HibernateTestApp.java
@@ -19,21 +19,22 @@ public class HibernateTestApp {
 	
 	public static void main(String[] args) {
 ////		new TimelineBAO().getTimelineData("781685528");
-		new Log();
-		System.setProperty("com.mchange.v2.log.MLog", "com.mchange.v2.log.FallbackMLog");
-		System.setProperty("com.mchange.v2.log.FallbackMLog.DEFAULT_CUTOFF_LEVEL", "INFO");
-		Logger.getLogger("org.hibernate").setLevel(Level.OFF);
-		
-		
-		Thread qThread;
-		Thread dbThread;
-		qThread = new Thread(new QueuePoller());
-        dbThread = new Thread(new DBPoller());
-     
-       qThread.start();
-       dbThread.start();
-		
-		new UserInfoBAO().getUserData("781685528");
+		new FollowersBAO().getFollowersData("781685528");
+//		new Log();
+//		System.setProperty("com.mchange.v2.log.MLog", "com.mchange.v2.log.FallbackMLog");
+//		System.setProperty("com.mchange.v2.log.FallbackMLog.DEFAULT_CUTOFF_LEVEL", "INFO");
+//		Logger.getLogger("org.hibernate").setLevel(Level.OFF);
+//		
+//		
+//		Thread qThread;
+//		Thread dbThread;
+//		qThread = new Thread(new QueuePoller());
+//        dbThread = new Thread(new DBPoller());
+//     
+//       qThread.start();
+//       dbThread.start();
+//		
+//		new UserInfoBAO().getUserData("781685528");
 
 	}
 }
